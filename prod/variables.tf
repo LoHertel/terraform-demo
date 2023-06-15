303c8831a17d16a776b71f722add42caaccb18c8
LoHertel <LoHertel@users.noreply.github.com>
2023-06-15 10:01:41 +0200

    add iac config for prod

---
+++ prod/variables.tf
@@ -0,0 +1,31 @@
+locals {
+  # hard coded for each environment
+  environment = "prod"
+
+  project          = var.project
+  region           = var.region
+  zone             = "${var.region}-${var.zone_suffix}"
+  name_suffix      = "${local.project}-${local.region}"
+  buckets = [
+    "${local.name_suffix}-data-lake",
+    "${local.name_suffix}-data-lake-anon"
+  ]
+}
+
+
+variable "project" {
+  description = "GCP Project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "Region for GCP resources. All available regions could be found here: https://cloud.google.com/storage/docs/locations#location-r"
+  default     = "us-east1"
+  type        = string
+}
+
+variable "zone_suffix" {
+  description = "Zone Suffix for Region. All available zones for a region could be found here: https://cloud.google.com/compute/docs/regions-zones#available"
+  default     = "d"
+  type        = string
+}
